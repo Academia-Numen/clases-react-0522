4d64e3e102379f56948f462dd0e94cd1b4f3695f
Aaron Ivan Amaro <aaronivanamaro@gmail.com>
2022-10-06 21:34:59 -0300

    avances clase 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const numeroDeClase = 2;
+  const clases = {
+    contenedor: 'container'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={clases.contenedor}>
+      Bienvenidos a la clase: {numeroDeClase}
+    </div> 
   );
 }
 
